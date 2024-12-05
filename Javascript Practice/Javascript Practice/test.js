73f60453e56fef9775fd3bebf57cfe383f3c28f6
Adrian Velazquez <Velazqueza0809@gmail.com>
2024-12-05 12:30:35 -0600

    Adding new directory/folder to start learning and practicing Javascript

---
+++ Javascript Practice/Javascript Practice/test.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log("This is to test my javascript as I am learning new topics about it! This message is being executed in a separate javascript file that is being reference in the html page.");
+// We used console.log() to print out any statement towards the user to see in the Developer Console section tab when they inspect the web browser. Try it out!
+
+let name = "Joe";
+let surname = "Doe";
