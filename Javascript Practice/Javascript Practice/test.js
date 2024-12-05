af023f863a4e6a9e7d4d9523e5aa534a02f32449
Adrian Velazquez <Velazqueza0809@gmail.com>
2024-12-05 13:11:19 -0600

    Adding various practices with comments for future revisions and studing

--- Javascript Practice/Javascript Practice/test.js
+++ Javascript Practice/Javascript Practice/test.js
@@ -2,5 +2,57 @@ console.log("Hello World!");
 console.log("This is to test my javascript as I am learning new topics about it! This message is being executed in a separate javascript file that is being reference in the html page.");
 // We used console.log() to print out any statement towards the user to see in the Developer Console section tab when they inspect the web browser. Try it out!
 
-let name = "Joe";
+// Practice using let variable types and printing them out.
+let name = "John";
 let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+// Declaring and reassigning variable.
+let age= 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+// Notice in the developer console tab in the webpage, when you inspect the int variable show up as green.
+
+// Practice with const. Const variables are used for when we don't want or need to reassign a variable ever, even if its changed later. If it is changed, the code will throw an error as shown in the example below.
+
+const Pi = 3.14; // Declare const variable
+// Pi = 10; // Try to reassign it and print it out on the console. This is commented out by default as it will prevent future tasks from running.
+console.log(Pi);
+// An error message should shown in the developer console tab in the webpage when inspecting this output.
+
+
+// Practice doing equations and printing in the console.log(function) form The Odin Project lessons.
+
+console.log((3+4)-76*(1+1));
+
+console.log(23+23+46-12+45);
+
+console.log((4+6+9) / 77);
+
+let a = 10;
+console.log("a =", a);
+
+console.log(9*a);
+
+let b = 7*a;
+
+console.log("b =", b);
+
+const max = 57;
+
+let actual = max-13;
+
+let percentage = actual / max;
+
+console.log("Percentage =",percentage);
+
+
+
+
+
+
